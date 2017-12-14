ef804dfed9e8a277983a3d86a52dfe6375756fe5
Vlado Grancaric <vlado.grancaric@redbubble.com>
2017-12-14 21:30:28 +1100

    Exposing the context via GraphQlResult

--- src/main/scala/com/redbubble/graphql/GraphQlResult.scala
+++ src/main/scala/com/redbubble/graphql/GraphQlResult.scala
@@ -7,22 +7,23 @@ import scala.collection.immutable.Seq
 
 sealed trait GraphQlResult {
   def underlying: Json
+  def context: Any
 }
 
 /**
   * The result of a successful GraphQL query execution.
   */
-final case class SuccessfulGraphQlResult(underlying: Json) extends GraphQlResult
+final case class SuccessfulGraphQlResult(underlying: Json, context: Any = Unit) extends GraphQlResult
 
 /**
   * The result of a failed GraphQL query execution, where the failure was prior to the execution (i.e. bad query).
   */
-final case class ClientErrorGraphQlResult(underlying: Json, errors: Seq[RegisteredError] = Seq.empty)
+final case class ClientErrorGraphQlResult(underlying: Json, errors: Seq[RegisteredError] = Seq.empty, context: Any = Unit)
     extends GraphQlResult
 
 /**
   * The result of a failed GraphQL query execution, where the failure was during the execution (e.g. internal,
   * downstream service, etc.).
   */
-final case class BackendErrorGraphQlResult(underlying: Json, errors: Seq[RegisteredError] = Seq.empty)
+final case class BackendErrorGraphQlResult(underlying: Json, errors: Seq[RegisteredError] = Seq.empty, context: Any = Unit)
     extends GraphQlResult
